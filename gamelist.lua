e062f9243e1f131465e38ebdeaf2486020b2b525
slayeryx <slayermaster211@gmail.com>
2026-07-05 16:25:11 -0400

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,5 +1,8 @@
 local Games = {
   [95082159892680] = "https://raw.githubusercontent.com/slayeryx/Aether/refs/heads/main/%2B1%20Speed%20Keyboard%20Escape.lua", -- +1 Speed Keyboard Escape
   [9584852943] = "https://raw.githubusercontent.com/slayeryx/Aether/refs/heads/main/%2B1%20Speed%20Keyboard%20Escape.lua", -- +1 Speed Keyboard Escape
+  [6447798030] = "https://raw.githubusercontent.com/slayeryx/ScriptGames/refs/heads/main/fnf.lua", -- Funky-Friday
+  [79268393072444] = "https://raw.githubusercontent.com/slayeryx/ScriptGames/refs/heads/main/Sell%20Lemons", -- Sell Lemons
+  [6839171747] = "https://raw.githubusercontent.com/slayeryx/ScriptGames/refs/heads/main/Door/loader.lua", -- Doors
 }
 return Games
