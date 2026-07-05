7d6940daf57062a909d4072dc4c2c12cf97cc7f8
slayeryx <slayermaster211@gmail.com>
2026-07-05 16:21:09 -0400

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [958452943] = "https://raw.githubusercontent.com/slayeryx/Aether/refs/heads/main/%2B1%20Speed%20Keyboard%20Escape.lua", -- +1 Speed Keyboard Escape
+  [95082159892680] = "https://raw.githubusercontent.com/slayeryx/Aether/refs/heads/main/%2B1%20Speed%20Keyboard%20Escape.lua", -- +1 Speed Keyboard Escape
+  [95082159892680] = "https://raw.githubusercontent.com/slayeryx/Aether/refs/heads/main/%2B1%20Speed%20Keyboard%20Escape.lua", -- +1 Speed Keyboard Escape
 }
-
 return Games
